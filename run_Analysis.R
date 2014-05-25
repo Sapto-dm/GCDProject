ce4cfe5386d483adfc230fe0e5d0f3fcf15e4304
Sapto-dm <saptarsi007@gmail.com>
2014-05-25 19:39:28 +0530

    added change directory

--- run_Analysis.R
+++ run_Analysis.R
@@ -2,15 +2,18 @@
 
 # Task 1: Merging of the training and testing data
 
+#Setting working directory
+setwd("F:/Tech/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset")
+
 #loading the required files
-X_train <- read.table("F:/Tech/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt", quote="\"")
-X_test <- read.table("F:/Tech/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt", quote="\"")
-y_train <- read.table("F:/Tech/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt", quote="\"")
-y_test <- read.table("F:/Tech/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt", quote="\"")
-subject_train <- read.table("F:/Tech/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt", quote="\"")
-subject_test <- read.table("F:/Tech/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt", quote="\"")
-features <- read.table("F:/Tech/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/features.txt", quote="\"")
-activity_labels <- read.table("F:/Tech/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt", quote="\"")
+X_train <- read.table("train/X_train.txt", quote="\"")
+X_test <- read.table("test/X_test.txt", quote="\"")
+y_train <- read.table("train/y_train.txt", quote="\"")
+y_test <- read.table("test/y_test.txt", quote="\"")
+subject_train <- read.table("train/subject_train.txt", quote="\"")
+subject_test <- read.table("test/subject_test.txt", quote="\"")
+features <- read.table("features.txt", quote="\"")
+activity_labels <- read.table("activity_labels.txt", quote="\"")
 
 #combining the features , activity , subject id for both training and testing set
 X_train<-cbind(X_train,y_train,subject_train)
@@ -42,3 +45,5 @@ names(datafinal)<-gsub("-",'.',names(datafinal))
 
 # Task 5: tidy data set with the average of each variable for each activity and each subject
 tidydata<-aggregate(.~ subject+activity, datafinal, mean)
+# wrting to tidydata file
+write.table(tidydata.'tidydata.txt')
